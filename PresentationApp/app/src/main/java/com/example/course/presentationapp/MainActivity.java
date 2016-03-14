c2ae06f2936a0e6f826ff03cac8fd73518276626
Vlad Muscurel <vlad.muscurel@mready.ro>
2016-03-14 16:33:36 +0200

    Created project, added initial design, added a sample toast on the home button click

---
+++ PresentationApp/app/src/main/java/com/example/course/presentationapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.course.presentationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Here we set the layout for our activity
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Let's go!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
